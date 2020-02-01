0d550b6f301410ed3fab86b0f7975cfd3f967c99
leguich1985 <leguich@mail.ru>
2020-02-01 22:04:04 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
+import { Navbar } from "./Navbar/Navbar";
 
 const GlobalStyle = createGlobalStyle`
   body {
+    margin: 0;
     font-family: 'Open Sans', sans-serif;
   }
   h1, h2,h3 {
@@ -14,8 +16,8 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   return (
     <>
-      <h1>Pizza Shop</h1>
       <GlobalStyle />
+      <Navbar />
       <div>Pizza</div>
     </>
   );
